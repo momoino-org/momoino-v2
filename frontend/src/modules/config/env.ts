855f7343b2f5267ef97bc8082fa0f5868cfb8681
Phong Chu <chucamphong1999@gmail.com>
2024-12-08 11:40:26 +0000

    ci: update frontend build process and enhance environment variable handling
    - Added ORIGIN_URL environment variable to the frontend build process in GitHub Actions.
    - Modified getEnv function to accept a default value, improving error handling for missing environment variables.

--- frontend/src/modules/config/env.ts
+++ frontend/src/modules/config/env.ts
@@ -1,12 +1,17 @@
 /**
  * Get an environment variable.
  * @param key - The environment variable key.
+ * @param defaultValue - The default value to return if the environment variable is not set.
  * @returns The environment variable value.
  */
-export function getEnv(key: string): string {
+export function getEnv(key: string, defaultValue?: string): string {
   const value = process.env[key];
 
   if (!value) {
+    if (defaultValue) {
+      return defaultValue;
+    }
+
     throw new Error(`${key} is not set`);
   }
 
