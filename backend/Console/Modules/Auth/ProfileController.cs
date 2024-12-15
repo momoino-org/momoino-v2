ea069e4e4b4475e5097a0da539dd42f5b344cd7d
Phong Chu <chucamphong1999@gmail.com>
2024-12-15 08:41:57 +0000

    refactor: improve Result handling, refine API responses, cleanup configs, enhance OpenAPI

--- backend/Console/Modules/Auth/ProfileController.cs
+++ backend/Console/Modules/Auth/ProfileController.cs
@@ -1,7 +1,7 @@
+using System.Net.Mime;
 using Common.Modules.Auth;
 using Common.Modules.Core;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Console.Modules.Auth;
@@ -21,7 +21,9 @@ public class ProfileController(UserService userService) : ControllerBase
     /// An Ok result containing an ApiResponse with the PrincipalUser object.
     /// </returns>
     [HttpGet]
-    public async Task<Ok<ApiResponse<PrincipalUser>>> GetProfileAsync()
+    [ProducesResponseType<ApiResponse<PrincipalUser>>(StatusCodes.Status200OK)]
+    [ProducesResponseType<ProblemDetails>(StatusCodes.Status400BadRequest)]
+    public async Task<IResult> GetProfileAsync()
     {
         // Calls the UserService to retrieve the PrincipalUser object asynchronously.
         PrincipalUser principalUser = await userService.GetPrincipalUserAsync();
